68c93ff3ccf8cb1687ae1091544a455968dc7b21
Jago128 <jagobaetxegarai@gmail.com>
2025-03-31 13:59:08 +0200

    Tests created for buy and purchase

---
+++ src/tests/TestBuy.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class TestBuy {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
