e1219c42ffa415bbb0ab01671372da24b363aabd
Jago128 <jagobaetxegarai@gmail.com>
2025-04-01 09:30:14 +0200

    e

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -18,100 +18,99 @@ public class LoginController {
 	}
 
 	/**[USERS]**/
-
 	public boolean verifyUser(User user) {
 		return dao.verifyUser(user);
-	};
+	}
 
 	public boolean verifyUserPassword(User user) {
 		return dao.verifyUserPassword(user);
-	};
+	}
 
 	public boolean verifyUserType(User user) {
 		return dao.verifyUserType(user);
-	};
+	}
 	
 	public User getUser(User user){
 		return dao.getUser(user);
-	};
+	}
 
 	public boolean registerUser(User user) {
 		return dao.registerUser(user);
-	};
+	}
 
 	/**[PRODUCTS]**/
 
 	public Map<String, Product> verifyProduct() {
 		return dao.verifyProduct();
-	};
+	}
 
 	public Product obtainProduct(String name) {
 		return dao.obtainProduct(name);
-	};
+	}
 
 	public boolean insertProd(Product prod) {
 		return dao.insertProd(prod);
-	};
+	}
 
 	public boolean deleteProd(String nom) {
 		return dao.deleteProd(nom);
-	};
+	}
 
 	public int checkStock(String nomItem, boolean type) {
 		return dao.checkStock(nomItem, type);
-	};
+	}
 
 	public Map<String, Product> showProdsOrderedByStock() {
 		return dao.showProdsOrderedByStock();
-	};
+	}
 
 	/**[COMPONENTS]**/
 
 	public Map<String, Comp> verifyComponent() {
 		return dao.verifyComponent();
-	};
+	}
 
 	public Comp obtainComponent(String name) {
 		return dao.obtainComponent(name);
-	};
+	}
 
 	public boolean insertComp(Comp comp) {
 		return dao.insertComp(comp);
-	};
+	}
 
 	public boolean deleteComp(String nom) {
 		return dao.deleteComp(nom);
-	};
+	}
 
 	public Map<String, Comp> showCompsOrderedByStock() {
 		return dao.showCompsOrderedByStock();
-	};
+	}
 
 	/**[PRODUCTS & COMPONENTS]**/
 
 	public void sellAndSubstract(String codUser, String nomItem, int amount, double price, boolean type) {
 		dao.sellAndSubstract(codUser, nomItem, amount, price, type);
-	};
+	}
 
 	public boolean restock(int code, int quantity, boolean type) {
 		return dao.restock(code, quantity, type);
-	};
+	}
 
 	/**[BRANDS]**/
 
 	public Map<String, Brand> verifyBrands() {
 		return dao.verifyBrands();
-	};
+	}
 
 	public int getBrandCode(String brandName) {
 		return dao.getBrandCode(brandName);
-	};
+	}
 
 	public Map<String, Product> showProductsBrand(String brand) {
 		return dao.showProductsBrand(brand);
-	};
+	}
 
 	public Map<String, Comp> showComponentsBrand(String brand) {
 		return dao.showComponentsBrand(brand);
-	};
+	}
 }
