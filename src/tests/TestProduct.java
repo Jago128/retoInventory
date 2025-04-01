c58f065c15f0430d3de93f65fa98cd8ef54d3266
Enchufe0 <sabatelvictor96@gmail.com>
2025-04-01 09:50:48 +0200

    sd

--- src/tests/TestProduct.java
+++ src/tests/TestProduct.java
@@ -4,19 +4,107 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
 
+
+import model.Product;
+import model.TypeP;
+
 class TestProduct {
+	
+	private Product product;
 
 	@BeforeEach
-	void setUp() throws Exception {
-	}
+    public void setUp() {
+        // Create an instance of Employee before each test
+        product = new Product(1, "Iphone X", TypeP.MOBILE, 1000, 100, 1);
+    }
 
 	@AfterEach
 	void tearDown() throws Exception {
+		product = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+    public void testGetCodP() {
+        assertEquals(1, product.getCodP());
+    }
+	
+    @Test
+    public void testSetCodP() {
+        product.setCodP(2);
+        assertEquals(2, product.getCodP());
+    }
+
+    @Test
+    public void testGetNameP() {
+        assertEquals("Iphone X", product.getNameP());
+    }
+
+    @Test
+    public void testSetNameP() {
+        product.setNameP("Samsung S24");
+        assertEquals("Samsung S24", product.getNameP());
+    }
+    
+    @Test
+    public void testGetTypeP() {
+        assertEquals(TypeP.MOBILE, product.getTypeP());
+    }
+	
+    @Test
+    public void testSetTypeP() {
+        product.setTypeP(TypeP.COMPUTER);
+        assertEquals(TypeP.COMPUTER, product.getTypeP());
+    }
+
+    @Test
+    public void testGetPrice() {
+        assertEquals(1000, product.getPrice());
+    }
+	
+    @Test
+    public void testSetPrice() {
+        product.setPrice(2);
+        assertEquals(2, product.getPrice());
+    }
+
+    @Test
+    public void testGetStock() {
+        assertEquals(100, product.getStock());
+    }
+	
+    @Test
+    public void testSetStock() {
+        product.setStock(2);
+        assertEquals(2, product.getStock());
+    }
+
+    @Test
+    public void testGetCodBrand() {
+        assertEquals(1, product.getCodBrand());
+    }
+	
+    @Test
+    public void testSetCodBrand() {
+        product.setCodBrand(2);
+        assertEquals(2, product.getCodBrand());
+    }
+
+    
+	@Test
+	void testDefaultConstructor() {
+		Product defaultProduct=new Product();
+		assertNull(defaultProduct.getCodP());
+		assertNull(defaultProduct.getNameP());
+		assertNull(defaultProduct.getTypeP());
+		assertNull(defaultProduct.getPrice());
+		assertNull(defaultProduct.getStock());
+		assertNull(defaultProduct.getCodBrand());
 	}
+	
+	@Test
+    public void testParameterizedConstructor() {
+        assertEquals(1, product.getCodP());
+        assertEquals("Iphone X", product.getNameP());
+    }
 
 }
