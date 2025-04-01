c2404e86ed4e403b5000631afeddfb823422b133
Jago128 <jagobaetxegarai@gmail.com>
2025-04-01 10:06:53 +0200

    e

--- src/windows/AddNewWindow.java
+++ src/windows/AddNewWindow.java
@@ -175,7 +175,7 @@ public class AddNewWindow extends JDialog implements ActionListener {
 
 	// Sets the color of the text fields true = WHITE | false = RED
 	public void setTextColor(JTextField field, boolean correct) {
-		if(correct) {
+		if (correct) {
 			field.setBackground(Color.WHITE);
 		} else {
 			field.setBackground(new Color(250, 128, 114));
