b4d65ab9b2af25609aa4c662453c6cfdb1cdfb45
Jago128 <jagobaetxegarai@gmail.com>
2025-04-01 09:09:59 +0200

    yeet

--- src/tests/TestComp.java
+++ src/tests/TestComp.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
 
-import model.Comp;
-import model.TypeC;
-import principal.Employee;
+import model.*;
 
 class TestComp {
 	private Comp comp;
@@ -14,33 +12,35 @@ class TestComp {
 	void setUp() throws Exception {
 		comp = new Comp ("Asus GT710",TypeC.GRAPHICS,5,5,81.99);
 	}
-	
-	@Test
-    public void testDefaultConstructor() {
-        Comp defaultEmployee = new Comp();
-        assertNull(defaultEmployee.getCodC());
-        assertNull(defaultEmployee.getNameC());
-        assertNull(defaultEmployee.getTypeC());
-        assertNull(defaultEmployee.getCodBrand());
-        assertNull(defaultEmployee.getStock());
-        assertNull(defaultEmployee.getPrice());
-    }
-
-    @Test
-    public void testParameterizedConstructor() {
-        assertEquals(1, comp.getCodC());
-        assertEquals("Asus GT710", comp.getNameC());
-        assertEquals(TypeC.GRAPHICS, comp.getTypeC());
-        assertEquals(5, comp.getCodBrand());
-        assertEquals(5, comp.getStock());
-        assertEquals(81.99, comp.getPrice());       
-    }
-	
 
 	@AfterEach
 	void tearDown() throws Exception {
 		comp=null;
 	}
+	
+	@Test
+	public void testDefaultConstructor() {
+		Comp defaultEmployee = new Comp();
+		assertNull(defaultEmployee.getCodC());
+		assertNull(defaultEmployee.getNameC());
+		assertNull(defaultEmployee.getTypeC());
+		assertNull(defaultEmployee.getCodBrand());
+		assertNull(defaultEmployee.getStock());
+		assertNull(defaultEmployee.getPrice());
+	}
+
+	@Test
+	public void testParameterizedConstructor() {
+		assertEquals(1, comp.getCodC());
+		assertEquals("Asus GT710", comp.getNameC());
+		assertEquals(TypeC.GRAPHICS, comp.getTypeC());
+		assertEquals(5, comp.getCodBrand());
+		assertEquals(5, comp.getStock());
+		assertEquals(81.99, comp.getPrice());       
+	}
+
+
+
 
 	@Test
 	void test() {
