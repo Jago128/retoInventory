dd19a4e92dd8f3feb9abc39d2aaf02179f7ac2a7
Jago128 <jagobaetxegarai@gmail.com>
2025-04-01 10:09:47 +0200

    e

--- src/windows/ProductWindow.java
+++ src/windows/ProductWindow.java
@@ -147,7 +147,7 @@ public class ProductWindow extends JDialog implements ActionListener {
 		case "MOBILES": // Uses the TreeMap ordered by the key as the name
 			if(!products.isEmpty()) {
 				for (Product p : products.values()){
-					if(p.getStock()>0 && p.getTypeP()==TypeP.MOBILE) {
+					if (p.getStock()>0 && p.getTypeP()==TypeP.MOBILE) {
 						modelName.addElement(p.getNameP());
 						modelPrice.addElement(p.getPrice()+" €");
 					}	
@@ -157,7 +157,7 @@ public class ProductWindow extends JDialog implements ActionListener {
 		case "COMPUTERS": // Creates a TreeMap ordered by the key as the price
 			if(!products.isEmpty()) {
 				for (Product p : products.values()){
-					if(p.getStock()>0 && p.getTypeP()==TypeP.COMPUTER) {
+					if (p.getStock()>0 && p.getTypeP()==TypeP.COMPUTER) {
 						modelName.addElement(p.getNameP());
 						modelPrice.addElement(p.getPrice()+" €");
 					}	
@@ -167,7 +167,7 @@ public class ProductWindow extends JDialog implements ActionListener {
 		default:
 			if(!products.isEmpty()) {
 				for (Product p : products.values()){
-					if(p.getStock()>0) {
+					if (p.getStock()>0) {
 						modelName.addElement(p.getNameP());
 						modelPrice.addElement(p.getPrice()+" €");
 					}	
