f718f93b2fc27fda5607b39ffb8676d2945b875a
MedievAlex <alexirazola@gmail.com>
2025-04-03 09:51:49 +0200

    Error Message added to password.

--- src/exception/IncorrectPasswordFormatException.java
+++ src/exception/IncorrectPasswordFormatException.java
@@ -5,6 +5,6 @@ public class IncorrectPasswordFormatException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public IncorrectPasswordFormatException() {
-		super("Your password must contain at minimum one letter and one number, and it mist be at least 8 chacraters long.");
+		super("Incorrect password format.");
 	}
 }
