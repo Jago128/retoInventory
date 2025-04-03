2cff574f54d7576511ead1206a389cb1714546ff
Jago128 <jagobaetxegarai@gmail.com>
2025-04-03 10:23:26 +0200

    yep this works

--- src/windows/SignInWindow.java
+++ src/windows/SignInWindow.java
@@ -131,7 +131,7 @@ public class SignInWindow extends JDialog implements ActionListener {
 	}
 	
 	public boolean passwordFormat(String password) {
-		Pattern pattern = Pattern.compile("^([\\w]{1,})([\\d]{1,}).{8,}$");
+		Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*\\w).{8,}$");
 		Matcher matcher = pattern.matcher(password);
 		return matcher.matches();
 	}
